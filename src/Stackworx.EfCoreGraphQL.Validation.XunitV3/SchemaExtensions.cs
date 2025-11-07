3709bb08a646decf93271b910434fba257d1f90c
Ciaran Liedeman <ciaran@stackworx.io>
2025-11-07 17:39:07 +0200

    Published Validation

---
+++ src/Stackworx.EfCoreGraphQL.Validation.XunitV3/SchemaExtensions.cs
@@ -0,0 +1,36 @@
+// ReSharper disable once CheckNamespace
+namespace HotChocolate;
+
+using Microsoft.EntityFrameworkCore.Metadata;
+using Stackworx.EfCoreGraphQL.Validation;
+using Xunit;
+
+public static class SchemaExtensions
+{
+    public static void ValidateDbContext(
+        this ISchema schema,
+        IModel model,
+        params IEntityType[] entityTypesToIgnore
+        )
+    {
+        var groupedErrors = EvaluateSchema
+            .Evaluate(schema, model)
+            .GroupBy(e => e.EntityType)
+            .ToDictionary(g => g.Key, g => g.ToList());
+
+        var actions = new List<Action>();
+
+        foreach (var (entityType, errors) in groupedErrors)
+        {
+            if (entityTypesToIgnore.Contains(entityType))
+            {
+                continue;
+            }
+
+            actions.Add(() => Assert.Empty(
+                errors.Select(e => $"GraphQL: {e.EntityType.Name}, DB: {e.ObjectType.Name}, Field: {e.Field.Name}, Message: {e.Message}")));
+        }
+        
+        Assert.Multiple(actions.ToArray());
+    }
+}
